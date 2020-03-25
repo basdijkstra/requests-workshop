4e3e636d709f28d8d168e0b76711a1b0f5c5376a
Bas Dijkstra <bas@ontestautomation.com>
2020-03-25 09:42:20 +0100

    Initial commit, first series of examples

---
+++ examples/examples_01.py
@@ -0,0 +1,34 @@
+import requests
+
+
+def test_get_user_with_id_1_check_status_code_equals_200():
+    response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+    assert response.status_code == 200
+
+
+def test_get_user_with_id_1_check_content_type_equals_json():
+    response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+    assert response.headers['Content-Type'] == "application/json; charset=utf-8"
+
+
+def test_get_user_with_id_1_check_encoding_equals_utf8():
+    response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+    assert response.encoding == "utf-8"
+
+
+def test_get_user_with_id_1_check_name_equals_leanne_graham():
+    response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+    response_body = response.json()
+    assert response_body["name"] == "Leanne Graham"
+
+
+def test_get_user_with_id_1_check_company_name_equals_romaguera_crona():
+    response = requests.get("https://jsonplaceholder.typicode.com/users/1")
+    response_body = response.json()
+    assert response_body["company"]["name"] == "Romaguera-Crona"
+
+
+def test_get_all_users_check_number_of_users_equals_10():
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    response_body = response.json()
+    assert len(response_body) == 10
