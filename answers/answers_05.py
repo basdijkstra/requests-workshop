35547418d8aef982dfdb1f7e39a0596563492daf
Bas Dijkstra <bas@ontestautomation.com>
2022-08-11 13:16:31 +0200

    Update section on working with XML response payloads to use lxml

--- answers/answers_05.py
+++ answers/answers_05.py
@@ -1,20 +1,21 @@
-import xml.etree.ElementTree as et
+from lxml import etree
+
 import requests
 
 
 # Exercise 5.1
 # Write a test that does the following:
-# Perform a GET to http://parabank.parasoft.com/parabank/services/bank/accounts/12345
+# Perform a GET to https://parabank.parasoft.com/parabank/services/bank/accounts/12345
 # Parse the response into an XML ElementTree
 # Check that the root element name is 'account'
 # Check that the root element has no attributes
 # Check that the root element has no text
 def test_check_root_of_xml_response():
     response = requests.get(
-        "http://parabank.parasoft.com/parabank/services/bank/accounts/12345"
+        "https://parabank.parasoft.com/parabank/services/bank/accounts/12345"
     )
-    response_body_as_xml = et.fromstring(response.content)
-    xml_tree = et.ElementTree(response_body_as_xml)
+    response_body_as_xml = etree.fromstring(response.content)
+    xml_tree = etree.ElementTree(response_body_as_xml)
     root = xml_tree.getroot()
     assert root.tag == "account"
     assert len(root.attrib) == 0
@@ -23,32 +24,32 @@ def test_check_root_of_xml_response():
 
 # Exercise 5.2
 # Write a test that does the following
-# Perform a GET to http://parabank.parasoft.com/parabank/services/bank/accounts/12345
+# Perform a GET to https://parabank.parasoft.com/parabank/services/bank/accounts/12345
 # Parse the response into an XML ElementTree
 # Find the customerId element in the tree
 # Check that the text of the customerId element is '12212'
 def test_check_specific_element_of_xml_response():
     response = requests.get(
-        "http://parabank.parasoft.com/parabank/services/bank/accounts/12345"
+        "https://parabank.parasoft.com/parabank/services/bank/accounts/12345"
     )
-    response_body_as_xml = et.fromstring(response.content)
-    xml_tree = et.ElementTree(response_body_as_xml)
+    response_body_as_xml = etree.fromstring(response.content)
+    xml_tree = etree.ElementTree(response_body_as_xml)
     first_name = xml_tree.find("customerId")
     assert first_name.text == "12212"
 
 
 # Exercise 5.3
 # Write a test that does the following
-# Perform a GET to http://parabank.parasoft.com/parabank/services/bank/customers/12212/accounts
+# Perform a GET to https://parabank.parasoft.com/parabank/services/bank/customers/12212/accounts
 # Parse the response into an XML ElementTree
 # Find all 'account' elements in the entire XML document
 # Check that there are more than 5 of these 'account' elements
 def test_check_number_of_accounts_for_12212_greater_than_five():
     response = requests.get(
-        "http://parabank.parasoft.com/parabank/services/bank/customers/12212/accounts"
+        "https://parabank.parasoft.com/parabank/services/bank/customers/12212/accounts"
     )
-    response_body_as_xml = et.fromstring(response.content)
-    xml_tree = et.ElementTree(response_body_as_xml)
+    response_body_as_xml = etree.fromstring(response.content)
+    xml_tree = etree.ElementTree(response_body_as_xml)
     accounts = xml_tree.findall(".//account")
     assert len(accounts) > 5
 
@@ -61,10 +62,10 @@ def test_check_number_of_accounts_for_12212_greater_than_five():
 #    find it on Google.)
 def test_use_xpath_for_more_sophisticated_checks():
     response = requests.get(
-        "http://parabank.parasoft.com/parabank/services/bank/customers/12212/accounts"
+        "https://parabank.parasoft.com/parabank/services/bank/customers/12212/accounts"
     )
-    response_body_as_xml = et.fromstring(response.content)
-    xml_tree = et.ElementTree(response_body_as_xml)
+    response_body_as_xml = etree.fromstring(response.content)
+    xml_tree = etree.ElementTree(response_body_as_xml)
     savings_accounts = xml_tree.findall(".//account/type[.='SAVINGS']")
     assert len(savings_accounts) > 1
     accounts_with_incorrect_customer_id = xml_tree.findall(
--- examples/examples_05.py
+++ examples/examples_05.py
@@ -1,13 +1,14 @@
-import xml.etree.ElementTree as et
+from lxml import etree
+
 import requests
 
 
 def test_check_root_of_xml_response():
     response = requests.get(
-        "http://parabank.parasoft.com/parabank/services/bank/customers/12212"
+        "https://parabank.parasoft.com/parabank/services/bank/customers/12212"
     )
-    xml_response_element = et.fromstring(response.content)
-    xml_response_tree = et.ElementTree(xml_response_element)
+    xml_response_element = etree.fromstring(response.content)
+    xml_response_tree = etree.ElementTree(xml_response_element)
     root = xml_response_tree.getroot()
     assert root.tag == "customer"
     assert root.text is None
@@ -15,10 +16,10 @@ def test_check_root_of_xml_response():
 
 def test_check_specific_element_of_xml_response():
     response = requests.get(
-        "http://parabank.parasoft.com/parabank/services/bank/customers/12212"
+        "https://parabank.parasoft.com/parabank/services/bank/customers/12212"
     )
-    xml_response_element = et.fromstring(response.content)
-    xml_response_tree = et.ElementTree(xml_response_element)
+    xml_response_element = etree.fromstring(response.content)
+    xml_response_tree = etree.ElementTree(xml_response_element)
     first_name = xml_response_tree.find("firstName")
     assert first_name.text == "John"
     assert len(first_name.attrib) == 0
@@ -27,9 +28,9 @@ def test_check_specific_element_of_xml_response():
 # https://docs.python.org/3/library/xml.etree.elementtree.html#elementtree-xpath
 def test_use_xpath_for_more_sophisticated_checks():
     response = requests.get(
-        "http://parabank.parasoft.com/parabank/services/bank/customers/12212"
+        "https://parabank.parasoft.com/parabank/services/bank/customers/12212"
     )
-    xml_response_element = et.fromstring(response.content)
-    xml_response_tree = et.ElementTree(xml_response_element)
+    xml_response_element = etree.fromstring(response.content)
+    xml_response_tree = etree.ElementTree(xml_response_element)
     address_children = xml_response_tree.findall(".//address/*")
     assert len(address_children) == 4
