b2ebffbb7093c63bedf4091bef177e85835ae7d0
Bas Dijkstra <bas@ontestautomation.com>
2025-10-15 09:04:15 +0200

    Fix exercise 3, add pipeline

--- exercises/exercises_03.py
+++ exercises/exercises_03.py
@@ -17,29 +17,3 @@ import requests
 # and checks that the response status code is 201
 # and that the new post id returned by the API is an integer
 # Use the isinstance(variable, type) function for this (Google is your friend!)
-
-
-# Exercise 3.3
-# Create a function create_billpay_for(name) that takes
-# an argument of type string containing a name and returns
-# an object that follows this structure:
-# {
-#   "name": <value of the name>,
-#   "address": {
-#     "street": "My street",
-#     "city": "My city",
-#     "state": "My state",
-#     "zipCode": "90210"
-#   },
-#   "phoneNumber": "0123456789",
-#   "accountNumber": 12345
-# }
-
-
-# Exercise 3.4
-# Write a test that POSTs the object created in 3.3 to
-# https://parabank.parasoft.com/parabank/services/bank/billpay?accountId=12345&amount=500
-# Supply a name of your own choice to the create_billpay_for() method
-# Make sure that the request header 'Accept' has value 'application/json' (Google ;)
-# Check that the response status code is 200 and
-# that the response body element 'payeeName' equals the name supplied to the method
